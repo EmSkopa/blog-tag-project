1ead3bd2bc0ad19bea3b5ab770ea4d321417c141
Emir Skopljak <emir.skopljak@jamax.ba>
2020-08-09 21:16:48 +0200

    Added automapper to services in startup

--- src/RubiconBlogProject/Startup.cs
+++ src/RubiconBlogProject/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,8 @@ namespace RubiconBlogProject
             var connectionString = Environment.GetEnvironmentVariable("RubiconDBConnection") ??
                                     Configuration.GetConnectionString("RubiconDBConnection");
             services.AddDbContext<RubiconDBContext>(opt => opt.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
+
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
